693088fbd82bd7cea6cf4c0253c856c83327ac78
Danil Galeev <digaleev@gmail.com>
2012-11-12 12:42:50 +0100

    changed admin interface, added model Comment

--- faq/admin.py
+++ faq/admin.py
@@ -2,19 +2,29 @@ from london.apps import admin
 from london import forms
 from london.apps.admin.modules import BaseModuleForm
 
-from models import Question
+from models import Question, Comment
 
 class FormQuestion(BaseModuleForm):
     class Meta:
         model = Question
 
-    def initialize(self):
-        self.fields['tags'].widget = forms.ListByCommaInput()
+#    def initialize(self):
+#        self.fields['tags'].widget = forms.ListByCommaInput()
+
+
+class FormComment(BaseModuleForm):
+    class Meta:
+        model = Comment
 
 class ModuleQuestion(admin.CrudModule):
     model = Question
-    list_display = ('title','answer','tags','is_published',)
+    list_display = ('modified_date', 'owner', 'text', 'is_published', 'status')
     form = FormQuestion
+
+class ModuleComment(admin.CrudModule):
+    model = Comment
+    list_display = ('modified_date', 'owner', 'text', 'parent_comment',)
+    form = FormComment
 
 class AppFAQ(admin.AdminApplication):
     title = 'FAQ'
